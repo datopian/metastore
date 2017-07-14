6be8fbe94ff989098208c0a958f73ef2bff01a00
zelima <irakli.mchedlishvili@datopian.com>
2017-07-14 13:16:13 +0400

    [config][xs]: no need for complicated config

--- server.py
+++ server.py
@@ -6,12 +6,12 @@ import metastore
 app = metastore.create()
 
 # Port to listen
-port = int(metastore.config.PORT)
+port = os.environ.get('PORT') or 5000
 if len(sys.argv) > 1:
     port = int(sys.argv[1])
 
 # Debug mode flag
-debug = metastore.config.DEBUG
+debug = True
 
 # Run application
 app.run(host='0.0.0.0', port=port, debug=debug)

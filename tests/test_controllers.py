96101dec3c8554d49c20913a83e158c0092a05f8
zelima <irakli.mchedlishvili@datopian.com>
2017-08-02 17:36:05 +0400

    [refactor/tests]: get old tests back and refactor
    * all old tests present
    * old testcases refatored not to use os_package_registry
    * all the tests refactored to follow new schema
    * simple mapping added as tests were failing (was done with os_package_registry earlier)
    refs #4

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -11,6 +11,17 @@ module = import_module('metastore.controllers')
 class SearchTest(unittest.TestCase):
 
     # Actions
+    MAPPING = {
+        'datahub': {
+            'type': 'object',
+            'properties': {
+                'owner': {
+                    "type": "string",
+                    "index": "not_analyzed",
+                }
+            }
+        }
+    }
 
     def setUp(self):
 
@@ -21,21 +32,67 @@ class SearchTest(unittest.TestCase):
         except NotFoundError:
             pass
         self.es.indices.create('datasets')
+        mapping = {'dataset': {'properties': self.MAPPING}}
+        self.es.indices.put_mapping(doc_type='dataset',
+                                    index='datasets',
+                                    body=mapping)
 
     def indexSomeRecords(self, amount):
         self.es.indices.delete(index='datasets')
         for i in range(amount):
             body = {
-                'id': True,
-                'dataset': i,
-                'model': 'str%s' % i,
-                'origin_url': {
+                'name': True,
+                'title': i,
+                'license': 'str%s' % i,
+                'datahub': {
                     'name': 'innername'
                 }
             }
-            self.es.index('datasets','dataset', body)
+            self.es.index('datasets', 'dataset', body)
         self.es.indices.flush('datasets')
 
+    def indexSomeRecordsToTestMapping(self):
+        for i in range(3):
+            body = {
+                'name': 'package-id-%d' % i,
+                'title': 'This dataset is number test%d' % i,
+                'datahub': {
+                    'owner': 'BlaBla%d@test2.com' % i,
+                },
+            }
+            self.es.index('datasets', 'dataset', body)
+        self.es.indices.flush('datasets')
+
+    def indexSomeRealLookingRecords(self, amount):
+        for i in range(amount):
+            body = {
+                'name': 'package-id-%d' % i,
+                'title': 'This dataset is number%d' % i,
+                'datahub': {
+                    'owner': 'The one and only owner number%d' % (i+1),
+                },
+                'loaded': True
+            }
+            self.es.index('datasets', 'dataset', body)
+        self.es.indices.flush('datasets')
+
+    def indexSomePrivateRecords(self):
+        i = 0
+        for owner in ['owner1', 'owner2']:
+            for private in [True, False]:
+                for content in ['cat', 'dog']:
+                    body = {
+                        'name': '%s-%s-%s' % (owner, private, content),
+                        'title': 'This dataset is number%d, content is %s' % (i, content),
+                        'datahub': {
+                            'owner': 'The one and only owner number%d' % (i+1),
+                            'ownerid': owner,
+                            'findability': private
+                        }
+                    }
+                    i += 1
+                    self.es.index('datasets', 'dataset', body)
+        self.es.indices.flush('datasets')
 
     # Tests
     def test___search___all_values_and_empty(self):
@@ -51,33 +108,36 @@ class SearchTest(unittest.TestCase):
 
     def test___search___filter_simple_property(self):
         self.indexSomeRecords(10)
-        self.assertEquals(len(module.search(None, {'model': ['"str7"']})), 1)
+        self.assertEquals(len(module.search(None, {'license': ['"str7"']})), 1)
 
     def test___search___filter_numeric_property(self):
         self.indexSomeRecords(10)
-        self.assertEquals(len(module.search(None, {'dataset': ["7"]})), 1)
+        self.assertEquals(len(module.search(None, {'title': ["7"]})), 1)
 
     def test___search___filter_boolean_property(self):
         self.indexSomeRecords(10)
-        self.assertEquals(len(module.search(None, {'id': ["true"]})), 10)
+        self.assertEquals(len(module.search(None, {'name': ["true"]})), 10)
 
     def test___search___filter_multiple_properties(self):
         self.indexSomeRecords(10)
-        self.assertEquals(len(module.search(None, {'model': ['"str6"'], 'dataset': ["6"]})), 1)
+        self.assertEquals(len(module.search(None, {'license': ['"str6"'], 'title': ["6"]})), 1)
 
     def test___search___filter_multiple_values_for_property(self):
         self.indexSomeRecords(10)
-        self.assertEquals(len(module.search(None, {'model': ['"str6"','"str7"']})), 2)
+        self.assertEquals(len(module.search(None, {'license': ['"str6"','"str7"']})), 2)
 
     def test___search___filter_inner_property(self):
         self.indexSomeRecords(7)
-        self.assertEquals(len(module.search(None, {"origin_url.name": ['"innername"']})), 7)
+        self.assertEquals(len(module.search(None, {"datahub.name": ['"innername"']})), 7)
 
     def test___search___filter_no_results(self):
-        self.assertEquals(len(module.search(None, {'model': ['"str6"'], 'dataset': ["7"]})), 0)
+        self.assertEquals(len(module.search(None, {'license': ['"str6"'], 'title': ["7"]})), 0)
 
     def test___search___filter_bad_value(self):
-        self.assertEquals(module.search(None, {'model': ['str6'], 'dataset': ["6"]}), None)
+        self.assertEquals(module.search(None, {'license': ['str6'], 'title': ["6"]}), None)
+
+    def test___search___filter_nonexistent_property(self):
+        self.assertEquals(module.search(None, {'license': ['str6'], 'boxing': ["6"]}), None)
 
     def test___search___returns_limited_size(self):
         self.indexSomeRecords(10)
@@ -90,3 +150,88 @@ class SearchTest(unittest.TestCase):
     def test___search___returns_results_from_given_index(self):
         self.indexSomeRecords(5)
         self.assertEquals(len(module.search(None, {'from':['3']})), 2)
+
+    def test___search___q_param_no_recs_no_results(self):
+        self.indexSomeRealLookingRecords(0)
+        self.assertEquals(len(module.search(None, {'q': ['"owner"']})), 0)
+
+    def test___search___q_param_some_recs_no_results(self):
+        self.indexSomeRealLookingRecords(2)
+        self.assertEquals(len(module.search(None, {'q': ['"writer"']})), 0)
+
+    def test___search___q_param_some_recs_some_results(self):
+        self.indexSomeRealLookingRecords(2)
+        results = module.search(None, {'q': ['"number1"']})
+        self.assertEquals(len(results), 1)
+
+    def test___search___q_param_some_recs_all_results(self):
+        self.indexSomeRealLookingRecords(10)
+        results = module.search(None, {'q': ['"dataset shataset"']})
+        self.assertEquals(len(results), 10)
+
+    def test___search___empty_anonymous_search(self):
+        self.indexSomePrivateRecords()
+        recs = module.search(None)
+        self.assertEquals(len(recs), 4)
+        ids = set([r['name'] for r in recs])
+        self.assertSetEqual(ids, {'owner1-False-cat',
+                                  'owner2-False-cat',
+                                  'owner1-False-dog',
+                                  'owner2-False-dog',
+                                  })
+
+    def test___search___empty_authenticated_search(self):
+        self.indexSomePrivateRecords()
+        recs = module.search('owner1')
+        ids = set([r['name'] for r in recs])
+        self.assertSetEqual(ids, {'owner1-False-cat',
+                                  'owner1-True-cat',
+                                  'owner2-False-cat',
+                                  'owner1-False-dog',
+                                  'owner1-True-dog',
+                                  'owner2-False-dog',
+                                  })
+        self.assertEquals(len(recs), 6)
+
+    def test___search___q_param_anonymous_search(self):
+        self.indexSomePrivateRecords()
+        recs = module.search(None, {'q': ['"cat"']})
+        self.assertEquals(len(recs), 2)
+        ids = set([r['name'] for r in recs])
+        self.assertSetEqual(ids, {'owner1-False-cat',
+                                  'owner2-False-cat',
+                                  })
+
+    def test___search___q_param_anonymous_search_with_param(self):
+        self.indexSomePrivateRecords()
+        recs = module.search(None, {'q': ['"cat"'], 'datahub.ownerid': ['"owner1"']})
+        self.assertEquals(len(recs), 1)
+        ids = set([r['name'] for r in recs])
+        self.assertSetEqual(ids, {'owner1-False-cat'})
+
+    def test___search___q_param_authenticated_search(self):
+        self.indexSomePrivateRecords()
+        recs = module.search('owner1', {'q': ['"cat"']})
+        ids = set([r['name'] for r in recs])
+        self.assertSetEqual(ids, {'owner1-False-cat',
+                                  'owner1-True-cat',
+                                  'owner2-False-cat',
+                                  })
+        self.assertEquals(len(recs), 3)
+
+    def test___search___q_param_with_similar_param(self):
+        self.indexSomeRecordsToTestMapping()
+        recs = module.search(None, {'q': ['"test2"']})
+        ids = set([r['name'] for r in recs])
+        self.assertSetEqual(ids, {'package-id-2'})
+        self.assertEquals(len(recs), 1)
+
+        recs = module.search(None, {'q': ['"dataset"'], 'datahub.owner': ['"BlaBla2@test2.com"']})
+        ids = set([r['name'] for r in recs])
+        self.assertSetEqual(ids, {'package-id-2'})
+        self.assertEquals(len(recs), 1)
+
+        recs = module.search(None, {'datahub.owner': ['"BlaBla2@test2.com"']})
+        ids = set([r['name'] for r in recs])
+        self.assertSetEqual(ids, {'package-id-2'})
+        self.assertEquals(len(recs), 1)
